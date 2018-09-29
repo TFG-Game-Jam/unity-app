072695e3267f312da2081e69154a3ee0259dd299
sadikneipp <sadi.neto13@gmail.com>
2018-09-29 09:14:27 -0300

    VR shooting

---
+++ Assets/DestroyOnColision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnColision : MonoBehaviour {
+
+	void OnCollisionEnter (Collision col)
+    {
+		Destroy(col.gameObject);
+		Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/Quiver.cs
+++ Assets/Scripts/Quiver.cs
@@ -6,16 +6,23 @@ public class Quiver : MonoBehaviour {
 	public GameObject arrowPrefab;
 
 
-	private GameObject _heldArrow;
+	private GameObject _heldLaser;
 
-	private void HoldArrow(GameObject arrow) {
-	_heldArrow = arrow;
-
-	// make a child of this object
-	_heldArrow.transform.SetParent(transform, false);
-	_heldArrow.transform.localPosition = new Vector3(0, 0, 1);
-	_heldArrow.transform.localEulerAngles = new Vector3(90, 0, 0);
+	private void AllignLaser(GameObject laser) {
+		_heldLaser = laser;
+		
+		// make a child of this object
+		_heldLaser.transform.SetParent(transform, false);
+		_heldLaser.transform.localPosition = new Vector3(0, 0, 1);
+		_heldLaser.transform.localEulerAngles = new Vector3(90, 0, 0);
 	}
+
+	private void ReleaseLaser(GameObject laser) {
+		// change the parent to the world
+		laser.transform.SetParent(null, true);
+		laser = null;
+	}
+
 	
 	// Use this for initialization
 	void Start () {
@@ -27,11 +34,16 @@ public class Quiver : MonoBehaviour {
 
   	// listen for click events
   		if (GvrControllerInput.ClickButtonDown) {
-    	CreateArrow();
+    	ShootLasers();
   		}
 	}
 
-	private void CreateArrow() {
-	Instantiate(arrowPrefab);
+	private void ShootLasers() {
+		GameObject laser =  Instantiate(arrowPrefab);
+		AllignLaser(laser);
+		ReleaseLaser(laser);
+		Rigidbody laserRigidbody = laser.GetComponent<Rigidbody>();
+		laserRigidbody.isKinematic = false;
+		laserRigidbody.AddRelativeForce( (new Vector3(0, -1, 0)) * 10, ForceMode.VelocityChange);
 	}
 }
