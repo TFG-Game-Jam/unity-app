00b1ab8f94527038951dacf8daf13185831adb3c
LucasHaug <lucas.haug@hotmail.com>
2018-09-29 09:32:56 -0300

    Add invaders

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// [System.Serializable]
+// public class MovementLimits
+// {
+//     public float xMin, xMax, zMin, zMax;
+// }
+
+public class EnemyMove : MonoBehaviour
+{
+    public float speed;
+    public float x_speed;
+    public float y_speed;
+
+    void FixedUpdate ()
+    {
+
+        Vector3 movement = new Vector3 (x_speed, 0.0f, y_speed);
+        GetComponent<Rigidbody> ().velocity = movement * speed;
+
+    }
+}
