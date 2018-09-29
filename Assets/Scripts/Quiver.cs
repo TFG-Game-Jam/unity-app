72dbe8b973279a19c42239963da5a53eaad87546
sadikneipp <sadi.neto13@gmail.com>
2018-09-29 05:24:19 -0300

    spawn fix-position props

---
+++ Assets/Scripts/Quiver.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quiver : MonoBehaviour {
+	public GameObject arrowPrefab;
+
+
+	private GameObject _heldArrow;
+
+	private void HoldArrow(GameObject arrow) {
+	_heldArrow = arrow;
+
+	// make a child of this object
+	_heldArrow.transform.SetParent(transform, false);
+	_heldArrow.transform.localPosition = new Vector3(0, 0, 1);
+	_heldArrow.transform.localEulerAngles = new Vector3(90, 0, 0);
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update() {
+
+  	// listen for click events
+  		if (GvrControllerInput.ClickButtonDown) {
+    	CreateArrow();
+  		}
+	}
+
+	private void CreateArrow() {
+	Instantiate(arrowPrefab);
+	}
+}
