d53b3e70b4d914ae7ea4be9c52805f37c3f767b1
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-09-29 10:59:01 -0300

    Add captain computer

---
+++ Assets/Scripts/ChangeUV.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeUV : MonoBehaviour {
+	const int ROOM_COUNT = 1;
+	Texture2D[] textures = new Texture2D[2 * ROOM_COUNT + 1];
+	int counter = 0;
+	int currentRoom = 0;
+	int[] roomStatus = new int[ROOM_COUNT]{0};
+
+	IEnumerator Start () {
+		string[] textureFiles = new string[] {
+			"UVs/Monitor/PropulsionWarning.png",
+			"UVs/Monitor/PropulsionError.png",
+			"UVs/Monitor/AllOK.png",
+		};
+		for (int i = 0; i < textureFiles.Length; i++) {
+        	textures[i] = new Texture2D(4, 4, TextureFormat.DXT1, false);
+			using (WWW www = new WWW(localPathToUri(textureFiles[i])))
+			{
+				yield return www;
+				www.LoadImageIntoTexture(textures[i]);
+			}
+		}
+		setRoomStatus(Room.PROPULSION, 1);
+	}
+
+	string localPathToUri(string path) {
+		String fullPath = Application.dataPath + "/" + path;
+		UriBuilder uriBuilder = new UriBuilder(fullPath);
+		uriBuilder.Scheme = "file";
+		return uriBuilder.ToString();
+	}
+
+	void FixedUpdate() {
+		counter++;
+		if (counter >= 80) {
+			counter = 0;
+			updateRoom();
+		}
+    }
+
+	void updateRoom() {
+		for (int i = (currentRoom + 1) % ROOM_COUNT; i != currentRoom; i = (i + 1) % ROOM_COUNT) {
+			if (roomStatus[i] != 0) {
+				currentRoom = i;
+				break;
+			}
+		}
+		GetComponent<Renderer>().material.mainTexture = roomStatus[currentRoom] == 0 ? textures[2 * ROOM_COUNT] :
+			(roomStatus[currentRoom] == 1 ? textures[2 * currentRoom] : textures[2 * currentRoom + 1]);
+	}
+
+	void setRoomStatus(Room room, int status) {
+		roomStatus[(int)room] = status;
+	}
+}
---
+++ Assets/Scripts/Rooms.cs
@@ -0,0 +1,3 @@
+public enum Room {
+    PROPULSION,
+};
