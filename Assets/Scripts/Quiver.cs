2098cbd68d792f2151a2a5c82a6f63898c401365
LucasHaug <39196309+LucasHaug@users.noreply.github.com>
2018-09-29 21:11:00 -0300

    Add files via upload

--- Assets/Scripts/Quiver.cs
+++ Assets/Scripts/Quiver.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class Quiver : MonoBehaviour {
 	public GameObject arrowPrefab;
 
-
 	private GameObject _heldLaser;
 
 	private void AllignLaser(GameObject laser) {
 		_heldLaser = laser;
-		
+				
 		// make a child of this object
 		_heldLaser.transform.SetParent(transform, false);
 		_heldLaser.transform.localPosition = new Vector3(0, 0, 1);
@@ -40,6 +39,25 @@ public class Quiver : MonoBehaviour {
 
 	private void ShootLasers() {
 		GameObject laser =  Instantiate(arrowPrefab);
+
+		bool loadWhite = Networking.playerActions.loadWhite;
+		bool loadGreen = Networking.playerActions.loadGreen;
+		bool loadCyan = Networking.playerActions.loadCyan;
+		bool loadPurple = Networking.playerActions.loadPurple;
+
+		if (loadWhite) {
+			laser.GetComponent<Renderer> ().material.color = new Color(1, 1, 1, 1); // white
+		}
+		else if (loadGreen) {
+			laser.GetComponent<Renderer> ().material.color = new Color(0, 1, 0, 1); // green
+		}
+		else if (loadCyan) {
+			laser.GetComponent<Renderer> ().material.color = new Color(0, 1, 1, 1); //cyan
+		}
+		else if (loadPurple) {
+			laser.GetComponent<Renderer> ().material.color = new Color(1, 0, 1, 1); // purple (magenta)
+		}
+		
 		AllignLaser(laser);
 		ReleaseLaser(laser);
 		Rigidbody laserRigidbody = laser.GetComponent<Rigidbody>();
