e9ac60182420b87e87242495b862284e610e6980
sadikneipp <sadi.neto13@gmail.com>
2018-09-30 02:00:13 -0300

    suicide mode

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,8 +8,8 @@ public class Spawner : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		float X_BOUND = 20;
-		float BLOCK_H  = 3;
-		float BLOCK_W  = 3;
+		float BLOCK_H  = 4;
+		float BLOCK_W  = 4;
 		float MAX_Z = 120;
 
 		float z = MAX_Z;
@@ -17,7 +17,7 @@ public class Spawner : MonoBehaviour {
 			float x = X_BOUND;
 			for (int j = 0; j < BLOCK_W; j++){
 				GameObject invader = Instantiate(invaderPrefab);
-				invader.transform.position = new Vector3(x, 0, z);
+				invader.transform.position = new Vector3(x, -2, z);
 				
 				x -= 2*X_BOUND/(BLOCK_W-1);
 			}
